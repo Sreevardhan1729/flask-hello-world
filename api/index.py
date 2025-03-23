710e6e022a0759389c6646f1d93c790a813f6677
Sreevardhan1729 <sreevardhan1729@gmail.com>
2025-03-23 22:22:52 +0530

    file-directory_error

--- api/index.py
+++ api/index.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import cross_origin
 
-from pdf_utils import pdf_to_text
-from preprocess import preprocess
+from api.pdf_utils import pdf_to_text
+from api.preprocess import preprocess
 import pickle
 app = Flask(__name__)
 
